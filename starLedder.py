a8256c886a7674787cc842ddcce03d4e68e44d00
Iwaa Liu <iwaaliu@users.noreply.github.com>
2016-04-15 14:23:12 -0500

    Create starLedder.py

---
+++ starLedder.py
@@ -0,0 +1,15 @@
+n=6
+for i in range(0,n-1):
+    print (" ")*(n-2-i),("#")*(i+1) #n-2 b/c there is a " " two outputs: output1 output2
+print ("#")*(n-0)
+
+
+#output
+     #
+    ##
+   ###
+  ####
+ #####
+######
+
+# in python3, print is a function, it can be done easier
