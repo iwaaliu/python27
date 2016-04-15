468af6f87210645243a5029b96035906a74b348a
Iwaa Liu <iwaaliu@users.noreply.github.com>
2016-04-15 15:23:39 -0500

    Create pe01.py

---
+++ pe01.py
@@ -0,0 +1,24 @@
+
+#brute force is simple, and complexity is O(n)
+s=0
+n=1000
+for i in range(1,1000):
+   if(i%3==0 or i%5==0) :
+       s+=i
+print s
+
+# O(1) method
+# sum(3*)+sum(5*)-overlap(3*,5*)
+#=sum(3*)+sum(5*)-sum([3*5]*)
+# sum(3*) = [3+n-mod(n,3)]/2*n/3
+
+n=n-1 # below n (1000), so we minus 1 as last possible access element.
+s3=(3+n-n%3)*(n/3)/2
+s5=(5+n-n%5)*(n/5)/2
+so=(15+n-n%15)*(n/3/5)/2 #put /2 at the end, or the intermidiate result might be odd, odd/2 will give wrong result
+s=s3+s5-so
+print s
+
+#output:
+#233168
+#233168
